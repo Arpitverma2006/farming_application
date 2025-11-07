6015d74688913f477af96937f8203493d549f98b
anshupal24 <anshupal28012005@gmail.com>
2025-11-07 23:05:42 +0530

    Anshu Pal

--- Frontend/forming_app/src/pages/login.jsx
+++ Frontend/forming_app/src/pages/login.jsx
@@ -3,7 +3,35 @@ import React from "react";
 function login() {
   return (
     <div>
-        {/* do code here  */}
+        {/* do code here  */
+        
+<body>
+  <div class="container">
+    <div class="login-box">           
+      <h2>Unnati Krishi Login</h2>
+      
+      <form id="loginForm">
+        <div class="input-box">
+          <i class="fas fa-envelope"></i>
+          <input type="email" id="email" required />
+          <label>Email</label>
+        </div>
+
+        <div class="input-box">
+          <i class="fas fa-lock"></i>
+          <input type="password" id="password" required />
+          <label>Password</label>
+        </div>
+
+        <button type="submit" class="btn">Login</button>
+      </form>
+    </div>
+  </div>
+
+  <script src="Login1.js"></script>
+</body>
+
+        }
     </div>
   );
 }
