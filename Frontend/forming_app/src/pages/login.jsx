8ced2cf75332b513745849a434175f24309ab66c
anshupal24 <anshupal28012005@gmail.com>
2025-11-07 23:54:57 +0530

    Anshu Pal

--- Frontend/forming_app/src/pages/login.jsx
+++ Frontend/forming_app/src/pages/login.jsx
@@ -4,7 +4,15 @@ function login() {
   return (
     <div>
         {/* do code here  */}
-        
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Unnati Krishi Login</title>
+  <link rel="stylesheet" href="Login1.css" />
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</head>
+       
 <body>
   <div class="container">
     <div class="login-box">           
